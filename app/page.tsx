01ac8b7b4c78291487f30444decd9ba173961ed7
satishgsonwane <satish.sonwane@gmail.com>
2025-01-13 14:09:38 +0530

    Bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 import ROISelector from "@/components/roi-selector";
-
-
+// commenting out the import of the ROISelector component
 
 export default async function Page() {
-
-
-
   return (
     <ROISelector />
   )
